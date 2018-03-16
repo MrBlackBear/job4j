da3642d70253367be26f7f794fee8f1fbf183d77
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-03-16 20:31:30 +0300

    3.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j.max;
+
+/**
+ * Max.
+ *
+ * @author Anatoli Shalobasov (proboostbiznes@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Method max.
+     *
+     * @param first  first number.
+     * @param second second param
+     * @return the largest number
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for max test tasks.
+ *
+ * @author Anatoli Shalobasov (proboostbiznes@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.max;
