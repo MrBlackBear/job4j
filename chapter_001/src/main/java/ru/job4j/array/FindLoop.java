0d57312e234be5641c032702559efe5a60310910
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-03-17 13:25:47 +0300

    5.1. Классический поиск перебором. [#33489]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * FindLoop.
+ *
+ * @author Anatoli Shalobasov (proboostbiznes@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoop {
+    /**
+     * Method calculate.
+     *
+     * @param data array of numbers.
+     * @param el   number which search in array
+     * @return index of number or -1
+     */
+    public int indexOf(int[] data, int el) {
+        int rsl = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * FindLoopTest.
+ *
+ * @author Anatoli Shalobasov (proboostbiznes@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoopTest {
+    /**
+     * Test indexOf.
+     */
+    @Test
+    public void whenElementFourThenIndexThree() {
+        FindLoop findLoop = new FindLoop();
+        int result = findLoop.indexOf(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 4);
+        assertThat(result, is(3));
+    }
+
+    /**
+     * Test indexOf.
+     */
+    @Test
+    public void whenNoNumberThenMinusOne() {
+        FindLoop findLoop = new FindLoop();
+        int result = findLoop.indexOf(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 114);
+        assertThat(result, is(-1));
+    }
+}
