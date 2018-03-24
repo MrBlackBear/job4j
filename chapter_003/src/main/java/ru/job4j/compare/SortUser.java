387d4d4473f2ca623b42e84333cfa9ca9a547e5b
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-03-24 18:20:05 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/compare/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/compare/SortUser.java
@@ -1,17 +1,32 @@
 package ru.job4j.compare;
 
-import java.util.Comparator;
-import java.util.List;
-
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
-    public Set<User> sort (List<User> users){
+    public Set<User> sort(List<User> users) {
         TreeSet<User> result = new TreeSet<>();
-        for (User user:users) {
+        for (User user : users) {
             result.add(user);
         }
         return result;
+    }
+
+    public List<User> sortNameLength(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            public int compare(User o1, User o2) {
+                return o1.getName().length() <= o2.getName().length() ? 1 : -1;
+            }
+        });
+        return list;
+    }
+
+    public List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            public int compare(User o1, User o2) {
+                final int result = o1.getName().compareTo(o2.getName());
+                return result != 0 ? result : Integer.compare(o1.getAge(), o2.getAge());
+            }
+        });
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/compare/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/compare/SortUserTest.java
@@ -1,13 +1,12 @@
 package ru.job4j.compare;
 
-import org.junit.Assert;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import static org.junit.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class SortUserTest {
@@ -15,16 +14,63 @@ public class SortUserTest {
     public void whenCompareThenReturnSortedResult() {
         SortUser sortUser = new SortUser();
         ArrayList<User> users = new ArrayList<>();
-        users.add(new User("ivan", 25));
-        users.add(new User("ivan44", 44));
-        users.add(new User("ivan12", 12));
-        users.add(new User("ivan3", 3));
+        User first = new User("ivan", 25);
+        User second = new User("ivan44", 44);
+        User third = new User("ivan12", 12);
+        User fourth = new User("ivan3", 3);
+        users.add(first);
+        users.add(second);
+        users.add(third);
+        users.add(fourth);
         Set<User> result = sortUser.sort(users);
         TreeSet<User> expected = new TreeSet<>();
-        expected.add(new User("ivan3", 3));
-        expected.add(new User("ivan12", 12));
-        expected.add(new User("ivan", 25));
-        expected.add(new User("ivan44", 44));
+        expected.add(fourth);
+        expected.add(third);
+        expected.add(first);
+        expected.add(second);
         assertTrue(result.equals(expected));
+    }
+
+    @Test
+    public void sortNameLength() {
+        SortUser sortUser = new SortUser();
+        ArrayList<User> users = new ArrayList<>();
+        User first = new User("ivan", 25);
+        User second = new User("ivan44", 44);
+        User third = new User("ivan12", 12);
+        User fourth = new User("ivan3", 3);
+        users.add(first);
+        users.add(second);
+        users.add(third);
+        users.add(fourth);
+        ArrayList<User> result = (ArrayList<User>) sortUser.sortNameLength(users);
+        ArrayList<User> expected = new ArrayList<>();
+        expected.add(second);
+        expected.add(third);
+        expected.add(fourth);
+        expected.add(first);
+        assertEquals(result, expected);
+    }
+
+
+    @Test
+    public void sortByAllFields() {
+        SortUser sortUser = new SortUser();
+        ArrayList<User> users = new ArrayList<>();
+        User first = new User("nick", 25);
+        User second = new User("nick", 44);
+        User third = new User("paulo", 12);
+        User fourth = new User("paulo", 3);
+        users.add(first);
+        users.add(second);
+        users.add(third);
+        users.add(fourth);
+        ArrayList<User> result = (ArrayList<User>) sortUser.sortByAllFields(users);
+        ArrayList<User> expected = new ArrayList<>();
+        expected.add(first);
+        expected.add(second);
+        expected.add(fourth);
+        expected.add(third);
+        assertEquals(result, expected);
     }
 }
