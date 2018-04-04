199a3dc49da3cd9525e6e285c6093e1761206601
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-04 21:37:24 +0400

    3. Конвертация двумерного массива в ArrayList и наоборот [#46543]

--- chapter_003/src/main/java/ru/job4j/search/ConvertList.java
+++ chapter_003/src/main/java/ru/job4j/search/ConvertList.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ConvertList {
@@ -15,37 +16,19 @@ public class ConvertList {
     }
 
     public int[][] toArray(List<Integer> list, int rows) {
-        Integer[] convertedArrayFromList = new Integer[list.size()];
-        convertedArrayFromList = list.toArray(convertedArrayFromList);
-        int rest = convertedArrayFromList.length % rows;
-        int newLength = convertedArrayFromList.length;
-        if (rest != 0) {
-            newLength = convertedArrayFromList.length + rows - rest;
-        }
-        Integer[] newLengthArray = new Integer[newLength];
-        System.arraycopy(convertedArrayFromList, 0, newLengthArray, 0, list.size());
-        int needSize = 0;
-        if (rest != 0) {
-            needSize = list.size() / rows + 1;
-        } else {
-            needSize = list.size() / rows;
-        }
-        int[][] result = new int[needSize][rows];
-        int[] ar;
-        int position = 0;
-        for (int i = 0; i < newLength;) {
-            ar = new int[rows];
+        Iterator<Integer> iterator = list.iterator();
+        int cols = list.size() / rows + (list.size() % rows == 0 ? 0 : 1);
+        int[][] array = new int[cols][rows];
+        for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
-                if (newLengthArray[i] == null) {
-                    ar[j] = 0;
-                    i++;
+                if (iterator.hasNext()) {
+                    array[i][j] = iterator.next();
                 } else {
-                    ar[j] = newLengthArray[i++];
+                    array[i][j] = 0;
                 }
             }
-            result[position++] = ar;
         }
-        return result;
+        return array;
     }
 
     public List<Integer> convert(List<int[]> list) {
