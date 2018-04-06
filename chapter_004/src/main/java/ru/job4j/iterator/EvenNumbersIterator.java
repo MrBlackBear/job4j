d24b7b92b87462dd4d477274f74994c4f42d1e70
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-06 12:32:33 +0400

    5.1.2. Создать итератор четные числа [#48248]

---
+++ chapter_004/src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,99 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * EvenNumbersIterator
+ *
+ * @author Anatoli Shalobasov (proboostbiznes@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class EvenNumbersIterator implements Iterator<Integer> {
+    /**
+     * Our array
+     */
+    private int[] array;
+    /**
+     * Count of even elements
+     */
+    int countEven;
+    /**
+     * INdex in array for which we iterate
+     */
+    int indexArray = 0;
+    /**
+     * Counts of elements which we found by method next in array
+     */
+    int countOfNeedElements = 0;
+
+    /**
+     * Constructor of class
+     *
+     * @param array our array
+     */
+    public EvenNumbersIterator(int[] array) {
+        this.array = array;
+        this.countEven = countOfEvenElements(array);
+    }
+
+    /**
+     * Method hasNext
+     *
+     * @return true if we have any even elements in array
+     */
+    @Override
+    public boolean hasNext() {
+        return countEven > countOfNeedElements;
+    }
+
+    /**
+     * Method next which throw exception if we have no more even elements or return even element from array
+     *
+     * @return next even element in array
+     * @throws NoSuchElementException if we have no more even elements
+     */
+    @Override
+    public Integer next() {
+        if (countEven == countOfNeedElements) {
+            throw new NoSuchElementException("No elements");
+        }
+        int element = 0;
+        for (int i = indexArray; i < array.length; i++) {
+            if (isEven(array[i])) {
+                countOfNeedElements++;
+                element = array[i];
+                indexArray = ++i;
+                break;
+            }
+        }
+        return element;
+    }
+
+    /**
+     * Method which retorn count of even elements
+     *
+     * @param array in which we search even elements
+     * @return counts of even elements
+     */
+    private int countOfEvenElements(int[] array) {
+        int count = 0;
+        for (int i : array) {
+            if (isEven(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Method which return even number or no
+     *
+     * @param number number which we check
+     * @return even number or no
+     */
+    private boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/iterator/EvenIteratorTest.java
@@ -0,0 +1,77 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class EvenIteratorTest {
+    /**
+     * Iterator which we test
+     */
+    private Iterator<Integer> it;
+
+    /**
+     * Iterator setUp before tests
+     */
+    @Before
+    public void setUp() {
+        it = new EvenNumbersIterator(new int[]{1, 2, 3, 4, 5, 6, 7});
+    }
+
+    /**
+     * Test for NoSuchElements
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnEvenNumbersSequentially() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    /**
+     * Test hasNext and next
+     */
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
+    }
+
+    /**
+     * Test has next and next
+     */
+    @Test
+    public void shouldReturnFalseIfNoAnyEvenNumbers() {
+        it = new EvenNumbersIterator(new int[]{1});
+        assertThat(it.hasNext(), is(false));
+    }
+
+    /**
+     * Test has next and next
+     */
+    @Test
+    public void allNumbersAreEven() {
+        it = new EvenNumbersIterator(new int[]{2, 4, 6, 8});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
+    }
+}
