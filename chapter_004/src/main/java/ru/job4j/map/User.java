747bf3abc61b37dc4b39c401326f8922007906c8
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-12 23:31:29 +0400

    1. Создать модель User [#999]

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * User
+ *
+ * @author Anatoli Shalobasov (proboostbiznes@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+    /**
+     * Name
+     */
+    private String name;
+    /**
+     * Number of children
+     */
+    private int children;
+    /**
+     * Date of birth
+     */
+    private Calendar birth;
+}
