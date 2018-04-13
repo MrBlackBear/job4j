29e426984bef89c23cab58d2707cc6304ffa7860
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-13 22:06:20 +0400

    4. Переопределить только equals [#48304]

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -37,14 +37,13 @@ public class User {
         this.birth = birth;
     }
 
-    /**
-     * HashCode of object
-     *
-     * @return hashCode
-     */
     @Override
-    public int hashCode() {
-
-        return Objects.hash(name, children, birth);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birth, user.birth);
     }
 }
