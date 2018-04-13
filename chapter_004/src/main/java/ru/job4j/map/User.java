afcf55485023a6136f8022cbf48edaf93510287a
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-13 22:09:07 +0400

    5. Перекрывать и equals и hashCode [#48305]

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -38,6 +38,12 @@ public class User {
     }
 
     @Override
+    public int hashCode() {
+
+        return Objects.hash(name, children, birth);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
