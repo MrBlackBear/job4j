570c9c83262121939855fca901bcdf6c418828f7
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-24 20:16:35 +0300

    2. Класс хранилища пользователей UserStorage [#1104]

---
+++ chapter_005/src/main/java/ru/job4j/monitor/UserStorage.java
@@ -0,0 +1,58 @@
+package ru.job4j.monitor;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ThreadSafe
+public class UserStorage {
+    @GuardedBy("this")
+    private List<User> users = new ArrayList<>();
+
+    public synchronized boolean add(User user) {
+        users.add(user);
+        return true;
+    }
+
+    ;
+
+    public synchronized boolean update(User user) {
+        boolean result = users.contains(user);
+        if (result) {
+            for (User us : users) {
+                if (us.equals(user)) {
+                    us.setAmount(user.getAmount());
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public synchronized boolean delete(User user) {
+        boolean result = users.contains(user);
+        if (result) {
+            users.remove(user);
+        }
+        return result;
+    }
+
+    public synchronized void transfer(int fromId, int toId, int amount) {
+        User from = null;
+        User to = null;
+        for (User us : users) {
+            if (us.getId() == fromId) {
+                from = us;
+            }
+            if (us.getId() == toId) {
+                to = us;
+            }
+        }
+        if (from != null && to != null && from.getAmount() >= amount) {
+            to.setAmount(to.getAmount() + amount);
+            from.setAmount(from.getAmount() - amount);
+        }
+    }
+}
