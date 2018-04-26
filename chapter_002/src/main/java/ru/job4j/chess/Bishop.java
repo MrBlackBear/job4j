28101dd95acffc33c7c308c42a59c3640723bd6f
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-26 19:41:41 +0300

    Каркас шахматной доски [#46415]

--- chapter_002/src/main/java/ru/job4j/chess/Bishop.java
+++ chapter_002/src/main/java/ru/job4j/chess/Bishop.java
@@ -12,30 +12,39 @@ public class Bishop extends Figure {
         int destX = dest.getX();
         int destY = dest.getY();
         int position = 0;
-        int compareX = Integer.compare(sourceX, destX);
-        int compareY = Integer.compare(sourceY, destY);
+        int x = Integer.compare(sourceX, destX);
+        int y = Integer.compare(sourceY, destY);
+        int size = Math.abs(destX - sourceX);
+        int vertical = y;
+        int horizontal = x;
         Cell[] result = new Cell[Math.abs(sourceX - destX)];
         diffCells(source, dest);
         outwardCells(source, dest);
         if (Math.abs(sourceX - destX) == Math.abs(sourceY - destY)) {
-            if (compareX == 1 && compareY == -1) {
-                for (int y = sourceY; y < destY; y++) {
-                    result[position++] = new Cell(--sourceX, ++sourceY);
+            for (int i = 0; i < size; i++) {
+                if (x == 1 && y == -1) {
+                    result[position++] = new Cell(sourceX - horizontal, sourceY - vertical);
+                    horizontal++;
+                    vertical--;
+                    continue;
                 }
-            }
-            if (compareX == -1 && compareY == -1) {
-                for (int y = sourceY; y < destY; y++) {
-                    result[position++] = new Cell(++sourceX, ++sourceY);
+                if (x == -1 && y == -1) {
+                    result[position++] = new Cell(sourceX - horizontal, sourceY - vertical);
+                    horizontal--;
+                    vertical--;
+                    continue;
                 }
-            }
-            if (compareX == -1 && compareY == 1) {
-                for (int x = sourceX; x < destX; x++) {
-                    result[position++] = new Cell(++sourceX, --sourceY);
+                if (x == -1 && y == 1) {
+                    result[position++] = new Cell(sourceX - horizontal, sourceY - vertical);
+                    horizontal--;
+                    vertical++;
+                    continue;
                 }
-            }
-            if (compareX == 1 && compareY == 1) {
-                for (int x = sourceX; x > destX; x--) {
-                    result[position++] = new Cell(--sourceX, --sourceY);
+                if (x == 1 && y == 1) {
+                    result[position++] = new Cell(sourceX - horizontal, sourceY - vertical);
+                    horizontal++;
+                    vertical++;
+                    continue;
                 }
             }
         } else {
