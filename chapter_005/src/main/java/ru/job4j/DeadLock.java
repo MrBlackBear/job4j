769deb98d060919b7d008b2ed516bdfab360d2b4
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-04-29 07:36:35 +0300

    Свитчер

---
+++ chapter_005/src/main/java/ru/job4j/DeadLock.java
@@ -0,0 +1,44 @@
+package ru.job4j;
+
+public class DeadLock {
+    public static void main(String[] args) {
+        Object first = new Object();
+        Object second = new Object();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (first){
+                    System.out.println(Thread.currentThread().getName() + "get first monitor");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "trying to get second monitor");
+                    synchronized (second){
+                        System.out.println(Thread.currentThread().getName() + "get second monitor");
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (second){
+                    System.out.println(Thread.currentThread().getName() + "get second monitor");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "trying to get first monitor");
+                    synchronized (first){
+                        System.out.println(Thread.currentThread().getName() + "get first monitor");
+                    }
+                }
+            }
+        }).start();
+    }
+}
