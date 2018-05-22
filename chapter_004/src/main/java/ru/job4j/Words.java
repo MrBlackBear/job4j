4938a5600585a39d5a5e61a806b0121d45b824a7
Anatoli Shalobasov <proboostbiznes@gmail.com>
2018-05-22 22:03:18 +0300

    Является ли слова аннаграмами[#52783]

--- chapter_004/src/main/java/ru/job4j/Words.java
+++ chapter_004/src/main/java/ru/job4j/Words.java
@@ -1,41 +1,33 @@
 package ru.job4j;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Words {
     public boolean isAnnagram(String first, String second) {
         char[] firstArray = first.toCharArray();
         char[] secondArray = second.toCharArray();
-        boolean result = true;
         HashMap<Character, Integer> hm1 = new HashMap<>();
         for (int i = 0; i < first.length(); i++) {
-            if (hm1.containsKey(firstArray[i])) {
-                Character key = firstArray[i];
-                Integer value = hm1.get(key);
-                hm1.put(key, value += 1);
-            } else {
-                hm1.put(firstArray[i], 1);
-            }
+            hm1.putIfAbsent(firstArray[i], 1);
+            hm1.computeIfPresent(firstArray[i], (key, value) -> value++);
         }
         for (int i = 0; i < firstArray.length; i++) {
+            hm1.computeIfPresent(secondArray[i], (key, value) -> value--);
             if (hm1.containsKey(secondArray[i])) {
-                if (hm1.get(secondArray[i]) > 1) {
-                    Character key = secondArray[i];
-                    Integer value = hm1.get(key);
-                    hm1.put(key, value -= 1);
-                } else {
+                if (hm1.get(secondArray[i]) == 1) {
                     hm1.remove(secondArray[i]);
                 }
-            } else {
-                result = false;
-                break;
             }
         }
-        return result && hm1.isEmpty();
+        return hm1.isEmpty();
     }
 
     public static void main(String[] args) {
         Words w = new Words();
         System.out.println(w.isAnnagram("top", "ort"));
+        System.out.println(w.isAnnagram("top", "opt"));
+        System.out.println(w.isAnnagram("topo", "opto"));
+        System.out.println(w.isAnnagram("topou", "ophto"));
     }
 }
